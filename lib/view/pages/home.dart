4b04697b1e04ca4973a0858b9c25a2785770ba62
chengche123 <1107640282@qq.com>
2021-05-03 18:01:44 +0800

    添加首页顶部导航

--- lib/view/pages/home.dart
+++ lib/view/pages/home.dart
@@ -1,5 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_comic/conf/view/color.dart';
 import 'package:flutter_comic/view/navigator/hi_navigator.dart';
+import 'package:flutter_comic/view/pages/home_tab_page.dart';
+import 'package:underline_indicator/underline_indicator.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({Key key}) : super(key: key);
@@ -9,21 +12,22 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage>
-    with AutomaticKeepAliveClientMixin {
+    with AutomaticKeepAliveClientMixin, TickerProviderStateMixin {
   RouteChangeListener listener;
-
+  TabController _controller;
   var tabs = ["推荐", "热门", "追播", "影视", "搞笑", "日常", "综合", "手机游戏", "短片·手书·配音"];
 
   @override
   void initState() {
     super.initState();
+    _controller = TabController(length: tabs.length, vsync: this);
     HiNavigator.getInstance().addListener(this.listener = (current, pre) {
       print('home:current:${current.rawPage}');
       print('home:pre:${pre?.rawPage}');
       if (widget == current.rawPage || current.rawPage is HomePage) {
-        print('home:onResume');
+        print('打开了首页:onResume');
       } else if (widget == pre?.rawPage || pre?.rawPage is HomePage) {
-        print('home:onPause');
+        print('首页:onPause');
       }
     });
   }
@@ -38,14 +42,19 @@ class _HomePageState extends State<HomePage>
   Widget build(BuildContext context) {
     super.build(context);
     return Scaffold(
-      appBar: AppBar(),
       body: Column(
         children: [
           Container(
             color: Colors.white,
             padding: EdgeInsets.only(top: 30),
-            child: Text("首页"),
+            child: _tabBar(),
           ),
+          Flexible(
+              child: TabBarView(
+                  controller: _controller,
+                  children: tabs.map((tab) {
+                    return HomeTabPage(name: tab);
+                  }).toList()))
         ],
       ),
     );
@@ -53,4 +62,26 @@ class _HomePageState extends State<HomePage>
 
   @override
   bool get wantKeepAlive => true;
+
+  ///自定义顶部tab
+  _tabBar() {
+    return TabBar(
+        controller: _controller,
+        isScrollable: true,
+        labelColor: Colors.black,
+        indicator: UnderlineIndicator(
+            strokeCap: StrokeCap.round,
+            borderSide: BorderSide(color: primary, width: 3),
+            insets: EdgeInsets.only(left: 15, right: 15)),
+        tabs: tabs.map<Tab>((tab) {
+          return Tab(
+              child: Padding(
+            padding: EdgeInsets.only(left: 5, right: 5),
+            child: Text(
+              tab,
+              style: TextStyle(fontSize: 16),
+            ),
+          ));
+        }).toList());
+  }
 }
